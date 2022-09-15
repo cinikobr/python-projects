91f5f19c74cb5407dfc2c8cad7712f015e98c266
cinikobr <98113633+cinikobr@users.noreply.github.com>
2022-09-14 23:31:01 -0400

    test2

--- gitupdate.sh
+++ gitupdate.sh
@@ -4,7 +4,7 @@ read message
 
 git add -A
 git commit -m "$message"
-git push https://cinikobr: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@github.com/cinikobr/python-projects.git
+git push
 
 function pause(){
  read -s -n 1 -p "Press any key to continue . . ."
