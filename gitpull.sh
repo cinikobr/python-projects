7914a1cc55a93722710b90c3b63b3fcabff937f2
cinikobr <98113633+cinikobr@users.noreply.github.com>
2022-09-14 23:32:09 -0400

    sh files

--- gitpull.sh
+++ gitpull.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git pull https://cinikobr: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@github.com/cinikobr/python-projects.git
+git pull
 
 function pause(){
  read -s -n 1 -p "Press any key to continue . . ."
